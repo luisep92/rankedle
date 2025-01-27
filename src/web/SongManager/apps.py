be079985ee91a14187d128be22a9b3c0b1de0352
Luis Escolano <luis@escolano.es>
2025-01-28 00:13:33 +0100

    Insert initial data on migrate

--- src/web/SongManager/apps.py
+++ src/web/SongManager/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SongmanagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'SongManager'
+
+    def ready(self):
+        import SongManager.signals
