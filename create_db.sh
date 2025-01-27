e087e4ec06045d2c1ea9ce8cd8c26257c31bb60f
Luis Escolano <luis@escolano.es>
2025-01-28 00:35:18 +0100

    Add file create_db.sh

---
+++ create_db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ./src/web/  
+
+echo "Creating migrations..."
+python manage.py makemigrations
+
+echo "Applying migrations..."
+python manage.py migrate
+
+echo "Create superuser for admin panel:"
+python manage.py createsuperuser
+
+
+echo "Run webserver..."
+python manage.py runserver
